8591b816587510177dba9ccf9b19fd9f854b7e1d
Mmakopa Matlhatse Kekana <Matlhatse95kk@gmail.com>
2025-03-26 08:47:12 +0200

    Add files via upload

---
+++ mylist.py
@@ -0,0 +1,16 @@
+my_list = [10, 20, 30, 40]
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)  
+my_list.append(40)
+
+my_list.insert(1,15)
+
+my_list.extend([50,60,70])
+
+my_list.pop(1)
+
+my_list.sort()
+
+("index of 30 is", my_list.index(30))
